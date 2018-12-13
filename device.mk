4b28bb5679bc4e507848fe35cdf194f47294f256
joelh <joelh@crdroid.net>
2018-12-13 13:54:06 -0500

    kiwi: Don't build Snap

--- device.mk
+++ device.mk
@@ -119,8 +119,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
     camera.msm8916 \
-    libmm-qcamera \
-    Snap
+    libmm-qcamera
 
 # Compatibility
 PRODUCT_PACKAGES += \
